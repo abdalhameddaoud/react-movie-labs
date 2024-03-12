607ef3edd1634e3d9f0ad5dd10aa7eea4fc8fcd4
Your Name <you@example.com>
2024-03-12 10:30:24 +0000

    Stored reviews in the context

--- movies/src/components/reviewForm/index.js
+++ movies/src/components/reviewForm/index.js
@@ -1,10 +1,15 @@
-import React, { useState } from "react";
 import Button from "@mui/material/Button";
 import TextField from "@mui/material/TextField";
 import MenuItem from "@mui/material/MenuItem";
 import Typography from "@mui/material/Typography";
 import Box from "@mui/material/Box";
 import { useForm, Controller } from "react-hook-form";
+import React, { useState, useContext } from "react";
+import { MoviesContext } from "../../contexts/moviesContext";
+import Snackbar from "@mui/material/Snackbar";
+import MuiAlert from "@mui/material/Alert";
+import { useNavigate } from "react-router-dom";
+
 
 const ratings = [
   {
@@ -28,6 +33,11 @@ const ratings = [
     label: "Terrible",
   },
 ];
+
+
+
+
+
 
 const styles = {
   root: {
@@ -56,16 +66,28 @@ const styles = {
   },
 };
 
+
+
+
 const ReviewForm = ({ movie }) => {
   const [rating, setRating] = useState(3);
-  
+  const context = useContext(MoviesContext);
+  const [open, setOpen] = useState(false); 
+const navigate = useNavigate();
   const defaultValues = {
     author: "",
     review: "",
     agree: false,
     rating: "3",
+
   };
   
+  const handleSnackClose = (event) => {
+    setOpen(false);
+    navigate("/movies/favorites");
+  };
+  
+
   const {
     control,
     formState: { errors },
@@ -77,10 +99,13 @@ const ReviewForm = ({ movie }) => {
     setRating(event.target.value);
   };
 
+ 
   const onSubmit = (review) => {
     review.movieId = movie.id;
     review.rating = rating;
-    console.log(review);
+    // console.log(review);
+    context.addReview(movie, review);
+    setOpen(true); // NEW
   };
 
   return (
@@ -88,7 +113,22 @@ const ReviewForm = ({ movie }) => {
       <Typography component="h2" variant="h3">
         Write a review
       </Typography>
-
+      <Snackbar
+        sx={styles.snack}
+        anchorOrigin={{ vertical: "top", horizontal: "right" }}
+        open={open}
+        onClose={handleSnackClose}
+      >
+        <MuiAlert
+          severity="success"
+          variant="filled"
+          onClose={handleSnackClose}
+        >
+          <Typography variant="h4">
+            Thank you for submitting a review
+          </Typography>
+        </MuiAlert>
+      </Snackbar>
       <form sx={styles.form} onSubmit={handleSubmit(onSubmit)} noValidate>
         <Controller
           name="author"
--- movies/src/contexts/moviesContext.js
+++ movies/src/contexts/moviesContext.js
@@ -4,7 +4,7 @@ export const MoviesContext = React.createContext(null);
 
 const MoviesContextProvider = (props) => {
   const [favorites, setFavorites] = useState( [] )
-
+  const [myReviews, setMyReviews] = useState( {} ) 
   const addToFavorites = (movie) => {
     let newFavorites = [];
     if (!favorites.includes(movie.id)){
@@ -15,6 +15,11 @@ const MoviesContextProvider = (props) => {
     }
     setFavorites(newFavorites)
   };
+
+  const addReview = (movie, review) => {
+    setMyReviews( {...myReviews, [movie.id]: review } )
+  };
+  //console.log(myReviews);
   
   // We will use this function in a later section
   const removeFromFavorites = (movie) => {
@@ -25,14 +30,15 @@ const MoviesContextProvider = (props) => {
 
   return (
     <MoviesContext.Provider
-      value={{
-        favorites,
-        addToFavorites,
-        removeFromFavorites
-      }}
-    >
-      {props.children}
-    </MoviesContext.Provider>
+    value={{
+      favorites,
+      addToFavorites,
+      removeFromFavorites,
+      addReview,
+    }}
+  >
+    {props.children}
+  </MoviesContext.Provider>
   );
 };
 
